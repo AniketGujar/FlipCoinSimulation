27e947ab873eee9335883e211fa509fffaa0cc0c
AniketGujar <gujaraniket7875@gmail.com>
2020-11-04 00:35:11 +0530

    Added: looping and Displaying H/T

--- coinFlip.sh
+++ coinFlip.sh
@@ -2,10 +2,13 @@
 
 echo "This problem displays the winner Heads or Tails"
 
+for (( i=0; i<=20; i++ ))
+do
 Toss=$((RANDOM%2))
 if [[ $Toss -eq 1 ]]
 then
-	echo Heads
+	echo "Heads"
 else
-	echo Tails
+	echo "Tails"
 fi
+done
