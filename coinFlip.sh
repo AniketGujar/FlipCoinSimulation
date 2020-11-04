67a5bb95ec725110f4fc4f57e68c2f7527138eb5
AniketGujar <gujaraniket7875@gmail.com>
2020-11-04 13:47:42 +0530

    Added: Tie and Greater than 2 Condition to win

--- coinFlip.sh
+++ coinFlip.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
-
 echo "This problem displays the winner Heads or Tails"
 
 for (( i=0; i<=50; i++ ))
 do
+
 Toss=$((RANDOM%2))
+
 if [[ $Toss -eq 1 ]]
+then
 	((h++))
-	if [[ $h -eq 21 ]]
-	then echo "Head Won 21 Times"
-	echo "Won by `expr $h - $t`"
-	break
-	fi
+
 else
 	((t++))
-	if [[ $t -eq 21 ]]
-	then echo "Tail won 21 Times"
-	echo "Won by `expr $t - $h`"
-	break
+fi
+
+if [[ $t -gt 20 || $h -gt 20 ]]
+then
+	if [[ $t -gt $h ]]
+	then
+		if [[ `expr $t - $h` -gt 2 ]]
+		then
+			echo "Tail Wins: No. of Tails = $t"
+			echo "No.of Head count = $h"
+		break
+		fi
+	elif [[ $t -lt $h ]]
+        then
+                if [[ `expr $h - $t` -gt 2 ]]
+                then
+                        echo "Head Wins: No. of Heads = $h"
+                        echo "No.of Tail count = $t"
+                break
+		fi
+	else
+		echo "TIE"
 	fi
 fi
 done
